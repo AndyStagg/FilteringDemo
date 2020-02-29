806e0d6d73119ef61df0f1e19583e9944a72f23b
andy stagg <atwinda@gmail.com>
2020-02-28 18:31:48 -0800

    Add LoadDummyParentItems

--- FilteringDemo/ViewModels/MainWindowViewModel.cs
+++ FilteringDemo/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,35 @@
-﻿using FilteringDemo.MVVM;
+﻿using FilteringDemo.Models;
+using FilteringDemo.MVVM;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace FilteringDemo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public ObservableCollection<>
+        public ObservableCollection<ParentItem> ParentItems { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ParentItems = new ObservableCollection<ParentItem>();
+
+            LoadDummyParentItems();
+        }
+
+        private void LoadDummyParentItems()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                ParentItems.Add(new ParentItem()
+                {
+                    Name = $"Parent Item {i}",
+                    CreatedOn = DateTime.Now.AddHours(i),
+                    IsActive = i % 2 == 0 ? true : false,
+                    Status = i % 2 == 0 ? ParentItemStatus.Status_Two : ParentItemStatus.Status_One,
+                    ChildItems = new List<string>() { $"Child one_{i}", $"Child two_{i}", $"Child three_{i}", $"Child four_{i}" }
+                });
+            }
+        }
     }
 }
